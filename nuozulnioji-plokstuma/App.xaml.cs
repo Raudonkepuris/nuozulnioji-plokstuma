ad1c426c3fd29e3d28c22740e7ad894e337b56bc
liut <liut@DESKTOP-4POCJ4B>
2024-12-05 20:11:06 +0200

    Add project files.

---
+++ nuozulnioji-plokstuma/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace nuozulnioji_plokstuma
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
